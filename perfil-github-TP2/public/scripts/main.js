3e8e67ed3bfe4f840c215f31aee6baf14847f1e5
Maria Moreira <maria.ecr.moreira1@gmail.com>
2024-06-27 12:36:27 -0300

    pagina inicial finalizada

--- perfil-github-TP2/public/scripts/main.js
+++ perfil-github-TP2/public/scripts/main.js
@@ -2,21 +2,8 @@ document.addEventListener('DOMContentLoaded', async () => {
     const GITHUB_API_URL = 'https://api.github.com';
     const GITHUB_USERNAME = 'mecrym';
     const MY_TOKEN = ''; // Token de autenticação GitHub
-    const LOCAL_JSON_FILENAME = 'db.json';
 
     // Função para carregar dados do arquivo JSON local
-    const fetchLocalJSONData = async (filename) => {
-        try {
-            const response = await fetch(`../${filename}`); // Caminho para db.json, assumindo que está um nível acima de scripts
-            if (!response.ok) {
-                throw new Error(`Erro na requisição: ${response.status}`);
-            }
-            return await response.json();
-        } catch (error) {
-            console.error(`Erro ao buscar dados do JSON local (${filename}): ${error.message}`);
-            return null;
-        }
-    };
 
     const fetchGitHubData = async (endpoint) => {
         try {
@@ -46,11 +33,14 @@ document.addEventListener('DOMContentLoaded', async () => {
             <img class="fotoperfil" src="${profile.avatar_url}" alt="${profile.name}">
             <h2 class="nome">${profile.name}</h2>
             <p class="descricao">${profile.bio || 'Sem descrição'}</p>
-            <p class="seguidores"><strong>Seguidores:</strong> ${profile.followers}</p>
-            <p class="localizacao"><strong>Localização:</strong> ${profile.location || 'Não informada'}</p>
-            <p class="github"><strong>GitHub: </strong> <a class="linkperfil" href="${profile.html_url}" target="_blank">mecrym</a></p>
-            <p class="linkedin"><strong>LinkedIn: </strong> <a class="linkperfil" href="https://www.linkedin.com/in/mecrym/" target="_blank">Maria Moreira</a></p>
-
+            <div class="info2">
+                <p class="seguidores"><strong><svg xmlns="http://www.w3.org/2000/svg" height="24" width="30" viewBox="0 0 640 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ffffff" d="M96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3zM504 312V248H440c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V136c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H552v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg></strong> ${profile.followers}</p>
+                <p class="localizacao"><strong><svg xmlns="http://www.w3.org/2000/svg" height="24" width="18" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ffffff" d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg></strong> ${profile.location || 'Não informada'}</p>
+            </div>
+                <div class="icones2">
+                <p class="github"><strong><svg xmlns="http://www.w3.org/2000/svg" height="25" width="22" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ffffff" d="M448 96c0-35.3-28.7-64-64-64H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96zM265.8 407.7c0-1.8 0-6 .1-11.6c.1-11.4 .1-28.8 .1-43.7c0-15.6-5.2-25.5-11.3-30.7c37-4.1 76-9.2 76-73.1c0-18.2-6.5-27.3-17.1-39c1.7-4.3 7.4-22-1.7-45c-13.9-4.3-45.7 17.9-45.7 17.9c-13.2-3.7-27.5-5.6-41.6-5.6s-28.4 1.9-41.6 5.6c0 0-31.8-22.2-45.7-17.9c-9.1 22.9-3.5 40.6-1.7 45c-10.6 11.7-15.6 20.8-15.6 39c0 63.6 37.3 69 74.3 73.1c-4.8 4.3-9.1 11.7-10.6 22.3c-9.5 4.3-33.8 11.7-48.3-13.9c-9.1-15.8-25.5-17.1-25.5-17.1c-16.2-.2-1.1 10.2-1.1 10.2c10.8 5 18.4 24.2 18.4 24.2c9.7 29.7 56.1 19.7 56.1 19.7c0 9 .1 21.7 .1 30.6c0 4.8 .1 8.6 .1 10c0 4.3-3 9.5-11.5 8C106 393.6 59.8 330.8 59.8 257.4c0-91.8 70.2-161.5 162-161.5s166.2 69.7 166.2 161.5c.1 73.4-44.7 136.3-110.7 158.3c-8.4 1.5-11.5-3.7-11.5-8zm-90.5-54.8c-.2-1.5 1.1-2.8 3-3.2c1.9-.2 3.7 .6 3.9 1.9c.3 1.3-1 2.6-3 3c-1.9 .4-3.7-.4-3.9-1.7zm-9.1 3.2c-2.2 .2-3.7-.9-3.7-2.4c0-1.3 1.5-2.4 3.5-2.4c1.9-.2 3.7 .9 3.7 2.4c0 1.3-1.5 2.4-3.5 2.4zm-14.3-2.2c-1.9-.4-3.2-1.9-2.8-3.2s2.4-1.9 4.1-1.5c2 .6 3.3 2.1 2.8 3.4c-.4 1.3-2.4 1.9-4.1 1.3zm-12.5-7.3c-1.5-1.3-1.9-3.2-.9-4.1c.9-1.1 2.8-.9 4.3 .6c1.3 1.3 1.8 3.3 .9 4.1c-.9 1.1-2.8 .9-4.3-.6zm-8.5-10c-1.1-1.5-1.1-3.2 0-3.9c1.1-.9 2.8-.2 3.7 1.3c1.1 1.5 1.1 3.3 0 4.1c-.9 .6-2.6 0-3.7-1.5zm-6.3-8.8c-1.1-1.3-1.3-2.8-.4-3.5c.9-.9 2.4-.4 3.5 .6c1.1 1.3 1.3 2.8 .4 3.5c-.9 .9-2.4 .4-3.5-.6zm-6-6.4c-1.3-.6-1.9-1.7-1.5-2.6c.4-.6 1.5-.9 2.8-.4c1.3 .7 1.9 1.8 1.5 2.6c-.4 .9-1.7 1.1-2.8 .4z"/></svg> </strong> <a class="linkperfil" href="${profile.html_url}" target="_blank">mecrym</a></p>
+                <p class="linkedin"><strong> <svg xmlns="http://www.w3.org/2000/svg" height="24" width="21" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ffffff" d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg></strong> <a class="linkperfil" href="https://www.linkedin.com/in/mecrym/" target="_blank">Maria Moreira</a></p>
+            </div>
         `;
     };
 
--- perfil-github-TP2/public/scripts/repos.js
+++ perfil-github-TP2/public/scripts/repos.js
@@ -35,7 +35,7 @@ document.addEventListener('DOMContentLoaded', async () => {
             repoDiv.classList.add('repositorio');
             repoDiv.innerHTML = `
                 <div class="repo-card">
-                    <h3>${repo.name}</h3>
+                    <h3 class="repo-name">${repo.name}</h3>
                     <div class="icones">
                         <div class="starfork">
                             <div class="star">
@@ -53,7 +53,10 @@ document.addEventListener('DOMContentLoaded', async () => {
                         </div>
                     </div>
                     <div class="repo-link">
-                        <a href="repo-template.html?repo=${repo.name}" target="_blank">Ver Detalhes</a>
+                        <a class="icone" href="repo-template.html?repo=${repo.name}" target="_blank">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="12" width="10.5" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#10002b" d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg>
+                        </a>
+                        <a class="link" href="repo-template.html?repo=${repo.name}" target="_blank">Ver Detalhes</a>
                     </div>
                 </div>
             `;
